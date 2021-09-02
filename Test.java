c7cc50237d86461745743f780d6e4ff538a9f3ee
isha <Isha_gupta@epam.com>
2021-09-02 18:17:19 +0530

    changes done

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
 
   public static void main(String args[]) {
-    System.out.println("Tested successfully");
+    System.out.println("Tested successfully after modification");
   }
 
 }
