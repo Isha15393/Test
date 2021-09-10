70900b51a33c89af3d3408e3c2fa18beb21fcfe2
Isha15393 <50434670+Isha15393@users.noreply.github.com>
2021-09-10 18:40:37 +0530

    updated

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
 
   public static void main(String args[]) {
-    System.out.println("Tested successfully after modification");
+    System.out.println("Tested successfully after modification modified");
   }
 
 }
