f91147e3b637e5e03a08ba7e369cab44d3fa15c0
isha153 <isha15393@gmail.com>
2021-09-10 18:19:23 +0530

    project added

---
+++ src/main/java/git/demo/App.java
@@ -0,0 +1,17 @@
+package git.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void displayMessage() {
+		System.out.println("My message displayed");
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        displayMessage();
+    }
+}
