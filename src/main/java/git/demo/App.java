227ca1bfaae3d472f297d54ef883e3dcc93b0712
isha153 <isha15393@gmail.com>
2021-09-10 18:25:29 +0530

    new method added

--- src/main/java/git/demo/App.java
+++ src/main/java/git/demo/App.java
@@ -9,9 +9,15 @@ public class App
 	public static void displayMessage() {
 		System.out.println("My message displayed");
 	}
+	
+	public static void displayMessage2() {
+		System.out.println("My message displayed modified");
+	}
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         displayMessage();
+        displayMessage2();
     }
 }
